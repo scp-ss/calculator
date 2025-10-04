060ae34c0c14eaee789931b2e07a017ff4b15f88
Muizz Nasir <muizznasir04@gmail.com>
2025-10-04 10:58:30 +0500

    Minor additinns

--- test.rs
+++ test.rs
@@ -10,7 +10,7 @@ fn main() {
 
     // This is due to the fact that: you wished for 4 wishes, it does not mean geni adds 1 wish to
     // ur total wishes, the geni replaces the current amount of wishes u have into 4,
-    // the geni accepts ur request, subtracts 1 wish (now u have 2), and gives u a total of 4 (replaces current amount of wishes into 4),
+    // the geni accepts ur request, subtracts 1 wish(now u have 2), and gives u a total of 4 (replaces current amount of wishes into 4),
     // he does not do x+1,
     // he does x = 4.
 }
